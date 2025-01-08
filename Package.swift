02fd4643a3b25aabf0b9a1b7f6263b1b0f02a3b2
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2025-01-08 13:58:00 +0900

    0.11.2 릴리즈

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,15 +12,15 @@ let package = Package(
             targets: ["RidiOfwModule", "RidiOfwModuleTarget"]),
     ],
     dependencies: [
-        
+        .package(url: "https://github.com/adison-ads/adison-offerwall-ios-sdk", exact: "3.9.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "RidiOfwModule",
-            url: "https://github.com/adison-ads/adison-ofw-module-ridi-ios/releases/download/0.11.0/RidiOfwModule.zip",
-            checksum: "2a7a338810a74795b24b9fff5a521a5686bb358f43aa814c63a90ab69342f5e0"
+            url: "https://github.com/adison-ads/adison-ofw-module-ridi-ios/releases/download/0.11.2/RidiOfwModule.zip",
+            checksum: "900c877f46efbcb9ee5782ddf1c2ed85ac60ed5739f99995584223fff210f33b"
         ),
         .target(
             name: "RidiOfwModuleTarget",
